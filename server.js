b33f720a1a9873a03cb05071256cc3027e2c40b1
Brian Boll <bboll@kent.edu>
2018-03-11 12:02:34 -0400

    Added 404 for HTTP requests

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+  .use((req, res) => res.status(404) )
+  .send('Not found')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
