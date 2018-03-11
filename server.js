b8395175cc59a0a2ba53525ecb1f9492ed037f6d
Brian Boll <bboll@kent.edu>
2018-03-11 16:21:38 -0400

    Server.js change

--- server.js
+++ server.js
@@ -8,27 +8,25 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.status(404) 
-  .send('Not found'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
+ws.on('connection', (ws) => {
   console.log('Client connected');
-  wss.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => console.log('Client disconnected'));
 });
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
+  ws.clients.forEach((client) => {
     client.send(new Date().toTimeString());
   });
 }, 10000);
 
-wss.on('open', function open() {
-  wss.send('Test message from server');
+ws.on('open', function open() {
+  ws.send('Test message from server');
 });
 
-wss.on('message', function incoming(data) {
+ws.on('message', function incoming(data) {
   console.log(data);
 });
